192183fb5ba96c76f566ae40b3ba2d3afa183c49
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2020-08-11 21:35:21 -0700

    added test console logs

---
+++ assets/js/game.js
@@ -0,0 +1,17 @@
+var playerName = window.prompt("What is your robot's name?");
+// Note the lack of quation marks around playerName
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message.");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+var name = "your name";
+console.log(name);
+
+function fight() {
+    window.alert("This is an alert! JavaScript is running!");
+}
+
+// fight();
